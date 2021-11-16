ad034bbee4547be7406c1025c8df50e6b49e45a0
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-11-16 15:42:31 -0700

    Agregar archivos de proyecto.

---
+++ Patolli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patolli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
