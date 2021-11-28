50cdb738f4de745a4373f1bc122ddbb7183389ac
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-11-28 14:05:46 -0700

    Arreglando dimensiones de los forms pricipales

--- Presentacion/Forms/ClientServerForm.Designer.cs
+++ Presentacion/Forms/ClientServerForm.Designer.cs
@@ -49,17 +49,17 @@ namespace Presentacion.Forms
             this.pnClientServer.Controls.Add(this.btnUnirse);
             this.pnClientServer.Controls.Add(this.btnCrearPartida);
             this.pnClientServer.Controls.Add(this.label1);
-            this.pnClientServer.Location = new System.Drawing.Point(112, 83);
+            this.pnClientServer.Location = new System.Drawing.Point(96, 72);
             this.pnClientServer.Name = "pnClientServer";
-            this.pnClientServer.Size = new System.Drawing.Size(400, 250);
+            this.pnClientServer.Size = new System.Drawing.Size(434, 272);
             this.pnClientServer.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Font = new System.Drawing.Font("Dubai Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(40, 123);
+            this.label2.Font = new System.Drawing.Font("Dubai Medium", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(57, 144);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 36);
             this.label2.TabIndex = 1;
@@ -70,9 +70,9 @@ namespace Presentacion.Forms
             this.btnUnirse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(75)))), ((int)(((byte)(8)))));
             this.btnUnirse.FlatAppearance.BorderSize = 0;
             this.btnUnirse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUnirse.Font = new System.Drawing.Font("Dubai", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUnirse.Font = new System.Drawing.Font("Dubai", 14.25F);
             this.btnUnirse.ForeColor = System.Drawing.Color.White;
-            this.btnUnirse.Location = new System.Drawing.Point(40, 162);
+            this.btnUnirse.Location = new System.Drawing.Point(50, 183);
             this.btnUnirse.Name = "btnUnirse";
             this.btnUnirse.Size = new System.Drawing.Size(313, 36);
             this.btnUnirse.TabIndex = 2;
@@ -85,9 +85,9 @@ namespace Presentacion.Forms
             this.btnCrearPartida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(75)))), ((int)(((byte)(8)))));
             this.btnCrearPartida.FlatAppearance.BorderSize = 0;
             this.btnCrearPartida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCrearPartida.Font = new System.Drawing.Font("Dubai", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCrearPartida.Font = new System.Drawing.Font("Dubai", 14.25F);
             this.btnCrearPartida.ForeColor = System.Drawing.Color.White;
-            this.btnCrearPartida.Location = new System.Drawing.Point(40, 70);
+            this.btnCrearPartida.Location = new System.Drawing.Point(50, 81);
             this.btnCrearPartida.Name = "btnCrearPartida";
             this.btnCrearPartida.Size = new System.Drawing.Size(313, 36);
             this.btnCrearPartida.TabIndex = 1;
@@ -98,16 +98,16 @@ namespace Presentacion.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Dubai Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(40, 31);
+            this.label1.Font = new System.Drawing.Font("Dubai Medium", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(57, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ser Host";
             // 
-            // frmClientServer
+            // ClientServerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
@@ -115,7 +115,7 @@ namespace Presentacion.Forms
             this.Controls.Add(this.pnClientServer);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "frmClientServer";
+            this.Name = "ClientServerForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClientServer";
             this.pnClientServer.ResumeLayout(false);
